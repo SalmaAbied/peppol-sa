d3039e976543f5e0667555824cad516646d2f1d8
SalmaAbied <114472834+SalmaAbied@users.noreply.github.com>
2024-04-19 09:26:33 +0200

    bezig met de vertaling van blogdata

--- qastan-sa/src/components/CtaBannerO.tsx
+++ qastan-sa/src/components/CtaBannerO.tsx
@@ -7,9 +7,9 @@ const CtaBannerO: React.FC = () => {
     <div className="bg-gray-800 text-white py-4 px-4 md:flex-row md:py-6 md:px-12 lg:px-24 xl:px-32">
         <div className='md:container mx-auto md:flex md:justify-between'>
       <div className="text-lg md:text-xl lg:text-2xl text-white">{t('Ondernemers.Cta.Title')}</div>
-      <button className="md:mt-0 mt-2 font-bold md:mr-0 mr-2 bg-legoOrange px-5 py-3 text-white hover:text-white hover:bg-legoOrange">
+      <a href='/Contact' className="md:mt-0 mt-2 font-bold md:mr-0 mr-2 bg-legoOrange px-5 py-3 text-white hover:text-white hover:bg-legoOrange">
       {t('Ondernemers.Cta.Button')}
-      </button>
+      </a>
       </div>
     </div>
   );
