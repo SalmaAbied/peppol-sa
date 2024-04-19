fc72709de44515f6933e8cb1d85e31965688cf4b
SalmaAbied <114472834+SalmaAbied@users.noreply.github.com>
2024-04-19 12:31:37 +0200

    bezig met filteren van de tabs

--- qastan-sa/src/components/BlogPosts.tsx
+++ qastan-sa/src/components/BlogPosts.tsx
@@ -20,8 +20,10 @@ const Blog: React.FC = () => {
     setCurrentPage(1);
   };
 
-  const filteredPosts = activeTab === `${t('Blog.Cat')}` ? blogPosts : blogPosts.filter(post => post.category === activeTab);
-
+  // const filteredPosts = activeTab === `${t('Blog.Cat')}` ? blogPosts : blogPosts.filter(post => post.category === activeTab);
+  const filteredPosts = activeTab === `${t('Blog.Cat')}` ? Object.values(blogPostTranslations).map(post => post[i18n.language]) : 
+  Object.values(blogPostTranslations).map(post => post[i18n.language]).filter(post => post.category === activeTab);
+  
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
@@ -32,7 +34,7 @@ const Blog: React.FC = () => {
     <section className="container mx-auto my-20">
       <div className='justify-center'>
         <p className="text-md uppercase  text-center">{t('Blog.Subtitle')}</p>
-        <h2 className="text-4xl md:text-5xl font-bold text-center">Blog</h2>
+        <h2 className="text-4xl md:text-5xl font-bold text-center">{t('Blog.Title')}</h2>
         <hr className="mx-auto w-12 h-1 outline-0 border-0 bg-legoLime block mt-4 mb-6" />
         <p className="text-center lg:text-lg max-w-3xl mx-auto mb-10">
           {t('Blog.Intro')}
@@ -40,23 +42,26 @@ const Blog: React.FC = () => {
       </div>
       <Tabs categories={categories} activeTab={activeTab} onTabClick={handleTabClick} />
       <div className="gap-8 flex flex-wrap justify-center">
-        {currentPosts.map((post, index) => (
-          <div key={index} className="w-full relative h-96 lg:w-96 bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-              <img src={post.imageUrl} alt={post.title} className="w-full h-96 object-cover transition duration-500" />
-              <div className="absolute inset-0 flex flex-col justify-end p-6">
-                <div className="bg-black bg-opacity-50 rounded-lg shadow-lg p-4">
-                  <h3 className="text-white text-lg font-semibold mb-2">{post.title}</h3>
-                  <div className='flex justify-between text-white font-semibold text-sm mb-0'>
-                  <p className="text-white font-bold text-sm mb-0">{post.subtitle}</p>
-                    <div>
-                      <Link to={`/Blog/${post.id}~${post.title.replace(' ', '-')}`} className='text-legoOrange flexborder-b border-legoOrange p-1'>{t('Blog.ReadMore')}</Link>
-                      <hr className='w-8 mx-auto border-legoOrange border-2'/>
+        {currentPosts.map((post, index) => {
+          const translation = blogPostTranslations[post.id]?.[i18n.language];
+          return (
+            <div key={index} className="w-full relative h-96 lg:w-96 bg-gray-800 rounded-lg overflow-hidden shadow-lg">
+                <img src={post.imageUrl} alt={translation?.title || post.title} className="w-full h-96 object-cover transition duration-500" />
+                <div className="absolute inset-0 flex flex-col justify-end p-6">
+                  <div className="bg-black bg-opacity-50 rounded-lg shadow-lg p-4">
+                    <h3 className="text-white text-lg font-semibold mb-2">{translation?.title || post.title}</h3>
+                    <div className='flex justify-between text-white font-semibold text-sm mb-0'>
+                      <p className="text-white font-bold text-sm mb-0">{translation?.subtitle || post.subtitle}</p>
+                      <div>
+                        <Link to={`/Blog/${post.id}~${post.title.replace(' ', '-')}`} className='text-legoOrange flexborder-b border-legoOrange p-1'>{t('Blog.ReadMore')}</Link>
+                        <hr className='w-8 mx-auto border-legoOrange border-2'/>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-          </div>
-        ))}
+            </div>
+          );
+        })}
       </div>
       <div className="flex justify-center mt-4">
         <button
--- qastan-sa/src/components/Tabs.tsx
+++ qastan-sa/src/components/Tabs.tsx
@@ -1,5 +1,6 @@
 // Blog tabs
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 interface TabsProps {
   categories: string[];
@@ -8,9 +9,10 @@ interface TabsProps {
 }
 
 const Tabs: React.FC<TabsProps> = ({ categories, activeTab, onTabClick }) => {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-wrap justify-center mb-8">
-      {['Alle', ...categories].map((category) => (
+      {[`${t('Blog.Cat')}`, ...categories].map((category) => (
         <button
           key={category}
           className={`mr-4 my-2 focus:outline-none ${activeTab === category ? 'bg-legoBlue text-white' : 'bg-[#eaf6f6] '} py-2 px-4 rounded-md`}
